c2b77cd8fecd9cb6a027824f3316f686ec16f0ea
ransiSMRK <164049919+MalithSamarakoon@users.noreply.github.com>
2026-03-23 15:41:37 +0530

    Created AuthController, two DTO classes and UserService

---
+++ backend/src/main/java/org/practicals/backend/controller/userManagement/AuthController.java
@@ -0,0 +1,23 @@
+package org.practicals.backend.controller.userManagement;
+
+
+import org.practicals.backend.dto.userManagement.RegistrationRequest;
+import org.practicals.backend.service.userManagement.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegistrationRequest request) {
+        userService.registerStudent(request);
+        return ResponseEntity.ok("Student registered successfully");
+    }
+}
